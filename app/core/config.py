2576d3d6824209263c56b1041f653f783c3dce41
Charly <charlyplanquettepro@gmail.com>
2025-11-26 20:46:50 +0100

    deployement railway

--- app/core/config.py
+++ app/core/config.py
@@ -1,14 +1,24 @@
 from pydantic_settings import BaseSettings
 
 class Settings(BaseSettings):
-    POSTGRES_USER: str
-    POSTGRES_PASSWORD: str
-    POSTGRES_DB: str
+    DATABASE_URL: str | None = None  # Railway la fournit
+    POSTGRES_USER: str | None = None
+    POSTGRES_PASSWORD: str | None = None
+    POSTGRES_DB: str | None = None
+
     SECRET_KEY: str
     ACCESS_TOKEN_EXPIRE_MINUTES: int = 60*24
 
     @property
-    def DATABASE_URL(self) -> str:
+    def database_url(self) -> str:
+        """
+        Retourne DATABASE_URL si définie (Railway),
+        sinon génère l'URL locale (développement).
+        """
+        if self.DATABASE_URL:
+            return self.DATABASE_URL
+
+        # fallback local
         return f"postgresql://{self.POSTGRES_USER}:{self.POSTGRES_PASSWORD}@localhost:5433/{self.POSTGRES_DB}"
 
     model_config = {
