8f47fc0d534eea33dc72f693a806746c21404399
Charly <charlyplanquettepro@gmail.com>
2025-12-04 13:30:23 +0100

    without pillow import

--- app/utils/image.py
+++ app/utils/image.py
@@ -1,7 +1,7 @@
 # import pytesseract
-from PIL import Image
+# from PIL import Image
 
 def image_to_text(image_path):
-    img = Image.open(image_path)
+    # img = Image.open(image_path)
     # text = pytesseract.image_to_string(img)
     return "Insdponible"
